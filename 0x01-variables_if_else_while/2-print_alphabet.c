849b0109f5488fe052b9a4f08ab64055144a601b
badt-guyz <elishaademuwagun@gmail.com>
2022-12-12 06:20:17 -0800

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print alpha in lowercase
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+char c;
+c = 'a';
+while
+	(c <= 'z') {
+		putchar(c);
+			c++;
+		}
+putchar('\n');
+return (0);
+}
