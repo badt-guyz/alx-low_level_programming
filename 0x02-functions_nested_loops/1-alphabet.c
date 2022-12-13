de6d554ad0845c3c3fec0e53c4043b38aca218ed
badt-guyz <elishaademuwagun@gmail.com>
2022-12-13 03:30:28 -0800

    alphbet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print alphbet - print alphabets in lowercase
+ */
+
+void print_alphabet(void);
+{char dn;
+for (dn = 'a'; dn <= 'z'; dn++)
+_putchar(dn);
+_putchar('\n');
+}
